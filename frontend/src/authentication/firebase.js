3cda440cc1a92781a7e6bd3d52545aace14c155f
Arif5577 <arifalamm33@gmail.com>
2022-10-21 20:48:33 +0600

    environment variable added

--- frontend/src/authentication/firebase.js
+++ frontend/src/authentication/firebase.js
@@ -2,22 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC79NklXcclh4RZMfrrxKDfQ2qNEq2Fcg8",
-  authDomain: "ema-john-2257d.firebaseapp.com",
-  projectId: "ema-john-2257d",
-  storageBucket: "ema-john-2257d.appspot.com",
-  messagingSenderId: "902882562971",
-  appId: "1:902882562971:web:7db7f7cde9a042a0fb073e",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
-/**
- * apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
- */
